5da62a963de7ea141285d73d95a1a71d2dc9a8de
GereSanta07 <geresanta@hotmail.com>
2024-10-23 22:25:05 -0500

    primer commit

--- src/ConsultaMoneda.java
+++ src/ConsultaMoneda.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class ConsultaMoneda {
+
+    Scanner lectura=new Scanner(System.in);
+
+    public Moneda ConsultaMoneda(String peso1,String peso2,double peso) {
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/16c84de311b4df3fe0d8d0bf/pair/" + peso1 + "/"+peso2+"/"+peso+"/");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Gson gson=new Gson();
+        return gson.fromJson(response.body(),Moneda.class);
+    }
+
+    public String Menu(){
+        return "*******************\n" +
+                "Sea bienvenido/a al Conversor de Moneda =\n\n" +
+                "1) Dólar => Peso argentino\n" +
+                "2) Peso argentino => Dólar\n" +
+                "3) Dólar => Real brasileño\n" +
+                "4) Real brasileño => Dólar\n" +
+                "5) Dólar => Peso colombiano\n" +
+                "6)Peso colombiano => Dólar\n" +
+                "7) Salir\n" +
+                "Elija una opcion válida:\n" +
+                "*******************";
+
+    }
+
+    public double IngresarValorAConvertir(){
+        System.out.println("Ingrese el valor que deseas convertir: ");
+        double monedas=lectura.nextDouble();
+        return monedas;
+    }
+
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Principal {
+    public static void main(String[] args) {
+        ConsultaMoneda consulta=new ConsultaMoneda();
+        Scanner lectura=new Scanner(System.in);
+        Moneda moneda =null;
+        int opcion;
+        do{
+            System.out.println(consulta.Menu());
+            opcion=lectura.nextInt();
+            double peso;
+            if(opcion==7){
+                break;
+            }
+            peso=consulta.IngresarValorAConvertir();
+            switch(opcion){
+                case 1:
+
+                    moneda=consulta.ConsultaMoneda("USD","ARS",peso);
+                    break;
+                case 2:
+
+                    moneda=consulta.ConsultaMoneda("ARS","USD",peso);
+                    break;
+                case 3:
+
+                    moneda=consulta.ConsultaMoneda("USD","BRL",peso);
+                    break;
+                case 4:
+
+                    moneda=consulta.ConsultaMoneda("BRL","USD",peso);
+                    break;
+                case 5:
+
+                    moneda=consulta.ConsultaMoneda("USD","COP",peso);
+                    break;
+                case 6:
+
+                    moneda=consulta.ConsultaMoneda("COP","USD",peso);
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+            if (moneda!=null){
+                System.out.println("El valor"+peso+" ["+moneda.base_code()+"] corresponde al valor final de =>>> "+moneda.conversion_result()+" ["+moneda.target_code()+"]");
+            }
+
+        }while(opcion!=7);
+    }
 }
